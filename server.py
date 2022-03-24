d67303c6f8c3b054a72eb8a575147f181edac619
Glitch (publiclinebot) <none>
2022-03-24 02:48:58 +0000

    🪗🐫 Updated with Glitch

--- server.py
+++ server.py
@@ -15,13 +15,16 @@ import requests
 
 
 app = Flask(__name__)
-#請在左方.env的檔案中加上： SECRET='這裡面請填你line後台的SECRET碼'
-scn=os.environ.get('SECRET')
+#請在左方.env的檔案中加上： SECRET ='這裡面請填你line後台的SECRET碼' 
+line_SECRET=os.environ.get('SECRET')
+
+#或是將line_SECRET直接代入line後台的SECRET碼 但不建議 因是公開的會被他人看到
+#line_SECRET = 'line後台的SECRET碼'
 
 # Channel Access Token
 line_bot_api = LineBotApi('這裡請填入line後台的Channel Access Token')
 # Channel Secret           
-handler = WebhookHandler(scn)
+handler = WebhookHandler(line_SECRET)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -37,7 +40,7 @@ def callback():
     return 'OK'
 
 # 處理訊息 
-#以下這段請先將webhook認證成功後 再將這段取消註解
+#以下這段請先將LINE後台的webhook認證成功後 再將這段取消註解
 '''
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
